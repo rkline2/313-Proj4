40b867f90cf4b30e9289e041bfc9e215b3c11b2f
rkline2 <rkline2@linux2.gl.umbc.edu>
2020-11-19 00:49:48 -0500

    version 0.1

--- drawBoard.c
+++ drawBoard.c
@@ -4,9 +4,31 @@
 #include <math.h>
 
 int main(){
-
-
-
-
+  const char DASH = '-';
+  const int LAST_RW = 16;
+  char x[16][3] = {" "," "," "," ",
+		   " "," "," "," ",
+		   " "," "," "," ",
+		   " "," "," "," "};
+  
+  for(int i=0; i<16; i++){
+    // last value of the row
+    if(((i+1) % 4) == 0){
+      printf("%s \n", x+i);
+      
+      if(i+1 != LAST_RW){
+	// print dashes
+	for (int j = 0; j<7; j++){       
+	    printf("%c", DASH);
+	}
+	printf("\n");
+      }
+    }
+    
+    // normal case 
+    else{
+    printf("%s|", x+i);
+    }
+  }
   return 0;
 }
