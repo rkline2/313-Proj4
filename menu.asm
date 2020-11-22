c0de8cc4cf452ea5420be067af58029554e5b2f4
rkline2 <rkline2@linux3.gl.umbc.edu>
2020-11-21 22:27:25 -0500

    version 1.2

--- menu.asm
+++ menu.asm
@@ -1,19 +1,16 @@
+	extern printf
 	extern scanf
+	
 	section .data
-new_line:	db 10
-intro:		db "Welcome to TIC-TAC-ASSEMBLY",10
-ask_route:	db "Pick an option from the menu below:",10
-err:		db "Invalid Input!",10
+intro:		db "Welcome to TIC-TAC-ASSEMBLY",0
+ask_route:	db "Pick an option from the menu below:",0
+err:		db "Invalid Input!",0
 	
-opt_1:		db "a - Easy",10
-opt_2:		db "b - Hard",10
-opt_3:		db "q - quit",10
-fmt_scan:	db '%c',0	; the scanf format
+opts:		db "a - Easy",10,"b - Hard",10,"q - quit", 10,0
+
+fmt:		db "%s",10,0
+fmt_scan:	db "%s",0	; the scanf format
 	
-len_i:		equ $-intro
-len_r:		equ $-ask_route
-len_e:		equ $-err
-len_opts:	equ $-opt_1
 
 	section .bss
 lett_buff:	resq	1
@@ -24,25 +21,33 @@ menu:
 	;; parameters goes as follow: rdi,rsi,rdx,rcx,r8,r9
 	;; rax value is returned
 	push rbp
-	;; print("Welcome to TIC-TAC-ASSEMBLY\n")
-	mov rax,1
-	mov rdi,1
-	mov rsi,intro		
-	mov rdx,len_i
-	syscall
+	mov rbp,rsp
 
-	;; print("Pick an option from the menu below:\n")
-	mov rax,1
-	mov rdi,1
+	;; printf("Welcome to TIC-TAC-ASSEMBLY\n")
+	mov rdi,fmt
+	mov rsi,intro
+	mov rax,0
+	call printf
+	
+	;; printf("Pick an option from the menu below:\n")
+	mov rdi,fmt
 	mov rsi,ask_route
-	mov rdx,len_i
-	syscall
+	mov rax,0
+	call printf
+
+	;; displays all the options  
+	mov rdi,fmt
+	mov rsi,opts
+	mov rax,0
+	call printf
 
 	;; scanf("%c")
 	mov rdi,fmt_scan
 	mov rsi,lett_buff
 	mov rax,0
 	
+
+	push rbp
 	call scanf
 	pop rbp
 
@@ -50,6 +55,7 @@ menu:
 	xor rbx,rbx
 	mov bl,[lett_buff]
 
+	
 	;; checks if input is valid
 	cmp bl,97		; lett_buff == a?
 	je quit
@@ -60,31 +66,23 @@ menu:
 	cmp bl,113		; lett_buff == q?
 	je quit
 
+
 	;; print("Invalid Input!")
-	mov rax,1
-	mov rdi,1
+	mov rdi,fmt
 	mov rsi,err
-	mov rdx,len_e
-	syscall
+	mov rax,0
+	call printf
+
+	pop rbp
+	
 
 	;; repeat until input is valid
 	jmp menu
 
-
-quit:
-	;; testing only 
-	mov rax,1
-	mov rdi,1
-	mov rsi,lett_buff
-	mov rdx,2
-	syscall
-
-	mov rax,1
-	mov rdi,1
-	mov rsi,new_line
-	mov rdx,1
-	syscall
 	
+quit:
+	pop rbp
+
 	;; rax = lett_buff
 	mov al,[lett_buff]
 	
