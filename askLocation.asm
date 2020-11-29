3d3a97c942940bd55efeaa5e50be38425f2dee74
rkline2 <rkline2@linux2.gl.umbc.edu>
2020-11-28 19:54:59 -0500

    Version 1.3.2

--- askLocation.asm
+++ askLocation.asm
@@ -1,23 +1,65 @@
+	extern printf
+	extern scanf
+	
 	section	.data
-loc_prompt:     db "Enter a location on the board 1-16"
+loc_prompt:     db "Enter a location on the board 1-16",0
+err:		db "Invalid Input!",0
+	
 len_p:          equ $-loc_prompt
+fmt:		db "%s",10,0
+fmt_scan:	db "%d",0
 
 	section .bss
-location:	resb 3
+location:	resq 3
 	section .text
 	global askLocation
 askLocation:
+	;; parameters go as follow: rdi,rsi,rdx,rcx,r8,r9
+	;; rax value is returned
+	push rbp
+	mov rbp,rsp
 
-	mov rax,1
-	mov rdi,1
+	;; print(loc_prompt)  
+	mov rdi,fmt
 	mov rsi,loc_prompt
-	mov rdx,len_p
-	syscall
-
 	mov rax,0
-	mov rdi,0
-	mov rsi,location
-	mov rdx,3
+	call printf
 
+	;; scanf("%d")
+	mov rdi,fmt_scan
+	mov rsi,location
+	mov rax,0
+
+	push rbp
+	call scanf
+	pop rbp
+
+	;; rbx = location
+	xor rbx,rbx
+	mov bl,[location]
+
+	pop rbp
+
+	;; (location < 1)? repeat : continue
+	cmp bl,1
+	jl repeat
+
+	;; (location > 16)? repeat : continue
+	cmp bl,16
+	jg repeat
+
+	;; return location
+	mov al,[location]
 	ret
 	
+repeat:
+	;; repeats invalid input 
+	push rbp
+	mov rdi,fmt
+	mov rsi,err
+	mov rax,0
+	
+	call printf
+	
+	pop rbp
+	jmp askLocation
